e7224031851c8d52b749479f483d9c4ad1a73f82
gonchuu <gonchu_10@hotmail.com>
2022-06-03 08:26:21 +0200

    cv

---
+++ my-cv/src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from "react";
+import "./App.css";
+// import { About, Experience, Hero } from "./components";
+// import { CV } from "./CV/CV";
+
+// const { hero, about, experience } = CV;
+
+const App = () => {
+  return (
+    <div className="App">
+      {/* <About about={about} />
+      <Experience experience={experience} />
+      <Hero hero={hero} /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ my-cv/src/CV/CV.js
@@ -0,0 +1,37 @@
+export const CV = {
+    hero: {
+        name: 'Gonzalo Guillén',
+        adress: 'C/Apolo 57',
+        city: 'Torrejón de Ardoz',
+        email: 'gonchu_10@hotmail.com',
+        image: 'https://pickaface.net/gallery/avatar/20121024_082235_4373_CV.png',
+        birthDate: '01/03/1988',
+        gitHub: 'https://github.com/Gonchuu',
+    },
+    education: [
+        {
+            name:'TAFAD',
+            date: '2008-2009',
+            where: 'Colegio Antonio Machado, Alcalá de Henares',
+        },
+        {
+            name:'Magisterio de Primaria',
+            date: '2010-2012',
+            where: 'Universidad Alcalá de Henares',
+        },
+    ],
+    experience: [
+        {
+        name: 'Socorrista',
+        date: '15/06/2009-15-9-2009',
+        where: 'Torrejón de Ardoz',
+        description: 'Socorrista en una piscina de una urbanización en Torrejón de Ardoz',
+        },
+        {
+            name: 'TIPSA',
+            date: ' 2012-2016',
+            where: 'Torrejón de Ardoz',
+            description:'grabador de datos y mozo de almacén',
+        },
+    ],
+};
